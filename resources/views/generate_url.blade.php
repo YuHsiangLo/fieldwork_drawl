b6acd38cb224eb018796ca32f2960f895d2395dc
Roger Yu-Hsiang Lo <b13472003@gmail.com>
2022-03-04 14:10:30 -0800

    Replaced space with %20

--- resources/views/generate_url.blade.php
+++ resources/views/generate_url.blade.php
@@ -9,7 +9,7 @@
         <div class="row justify-content-center">
             <div class="col-md-10">
                 <div>Please pass the following URL to the consultant:</div>
-                <strong>https://fieldworkdrawl.linguistics.ubc.ca/record/create?date={{$elicitation_date}}&elicitor={{$elicitor_name}}&consultant={{$consultant_name}}&zoom={{$zoom_link}}</strong>
+                <strong>https://fieldworkdrawl.linguistics.ubc.ca/record/create?date={{$elicitation_date}}&elicitor={{str_replace(' ', '%20', $elicitor_name)}}&consultant={{str_replace(' ', '%20', $consultant_name)}}&zoom={{$zoom_link}}</strong>
             </div>
         </div>
     </div>
