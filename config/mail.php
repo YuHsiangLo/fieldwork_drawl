3510866116167d9048517f22fc713036b08569df
Roger Yu-Hsiang Lo <b13472003@gmail.com>
2024-10-01 19:24:52 -0700

    Added sparkpost-related code

--- config/mail.php
+++ config/mail.php
@@ -34,6 +34,10 @@ return [
     */
 
     'mailers' => [
+        'sparkpost' => [
+            'transport' => 'sparkpost'
+        ],
+
         'smtp' => [
             'transport' => 'smtp',
             'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
